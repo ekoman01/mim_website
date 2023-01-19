eb79b49b2c05aa1392b9bfcbea3c097eaaf20f53
Tijmen <tijmen.koetse@student.howest.be>
2023-01-19 23:54:00 +0100

    animations improved+floating

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ import '/css/style.css'
 
 gsap.registerPlugin(ScrollTrigger);
 
-const appleNumCols = 8;
-const appleStepPercentageX = 1 / appleNumCols;
-const applePositions = [
+const harmonicaNumCols = 8;
+const harmonicaStepPercentageX = 1 / harmonicaNumCols;
+const harmonicaPositions = [
   { x: 0 },
   { x: 1 },
   { x: 2 },
@@ -18,23 +18,42 @@ const applePositions = [
 const init = () => {
   const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: ".section-with-apple",
+      trigger: ".part_one",
       scrub: true,
-      markers: true,
-      pin: ".apple",
+      //markers: true,
+      pin: ".harmonica",
     },
   });
 
-  const apple = {
-    step: 0
+  const harmonica = {
+    step: 0,
   };
-  const $appleImage = document.querySelector('.apple__image');
+  const $harmonicaImage = document.querySelector(".harmonica__image");
 
-  tl.to(apple, { step: applePositions.length-1, duration: 5, ease: SteppedEase.config(applePositions.length-1), onUpdate: () => {
-    console.log(apple.step);
-    const applePosition = applePositions[apple.step];
-    $appleImage.style.transform = `translate(${-100 * applePosition.x * appleStepPercentageX}%`;
+  tl.to(harmonica, { step: harmonicaPositions.length-1, duration: 5, ease: SteppedEase.config(harmonicaPositions.length-1), onUpdate: () => {
+    console.log(harmonica.step);
+    const harmonicaPosition = harmonicaPositions[harmonica.step];
+    $harmonicaImage.style.transform = `translate(${-100 * harmonicaPosition.x * harmonicaStepPercentageX}%`;
   }})
+
+  tl.fromTo(".intro", { opacity: 0 }, { opacity: 1 }, ">");
+  tl.fromTo(".harmonica", { y: 0 }, { y: 100, delay: 2 }), ">10";
+  tl.fromTo(".harmonica", { opacity: 1 }, { opacity: 0 }), ">";
+
+  const tlParts = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".parts",
+      scrub: true,
+      //markers: true,
+      start: "top 80%",
+      end: "bottom 85%",
+    },
+  });
+  tlParts.to(".bottom", { x: 150 }, "<");
+  tlParts.to(".draw", { x: 75 }, "<");
+  tlParts.to(".blow", { x: -75 }, "<");
+  tlParts.to(".top", { x: -150 }, "<");
+
 };
 
 init();
