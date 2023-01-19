9253670405d1b6490ebfb6f38cafed6695e01cb5
Tijmen <tijmen.koetse@student.howest.be>
2023-01-19 17:06:48 +0100

    header animation improved

--- scripts/main.js
+++ scripts/main.js
@@ -2,64 +2,39 @@ import '/css/style.css'
 
 gsap.registerPlugin(ScrollTrigger);
 
-let frame_count, offset_value;
+const appleNumCols = 8;
+const appleStepPercentageX = 1 / appleNumCols;
+const applePositions = [
+  { x: 0 },
+  { x: 1 },
+  { x: 2 },
+  { x: 3 },
+  { x: 4 },
+  { x: 5 },
+  { x: 6 },
+  { x: 7 },
+];
 
-if (window.screen.width >= 600) {
-  frame_count = 7;
-  offset_value = 260;
-}
-else {
-  frame_count = 7;
-  offset_value = 160;
-}
+const init = () => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".section-with-apple",
+      scrub: true,
+      markers: true,
+      pin: ".apple",
+    },
+  });
 
+  const apple = {
+    step: 0
+  };
+  const $appleImage = document.querySelector('.apple__image');
 
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".scene",
-    start: "top top",
-    end: "+=" + frame_count * offset_value,
-    //markers: { fontSize: "25px", fontWeight: "bold" },
-    pin: true,
-    scrub: true,
-  },
-});
-tl.to(
-  ".viewer",
-  {backgroundPosition: -offset_value * frame_count * 2 + "px 50%",
-  ease: "steps(" + frame_count + ")", duration: 5}
-);
-tl.fromTo(".intro", { opacity: 0 }, { opacity: 1 }, ">0.5");
-tl.fromTo(".intro", { opacity: 1 }, { opacity: 0 }, ">2");
-tl.to(".viewer", { background: 0, duration: 0 }, ">1");
-tl.fromTo(".parts", { opacity: 0, duration: 0 }, { opacity: 1, duration: 0 }, ">");
+  tl.to(apple, { step: applePositions.length-1, duration: 5, ease: SteppedEase.config(applePositions.length-1), onUpdate: () => {
+    console.log(apple.step);
+    const applePosition = applePositions[apple.step];
+    $appleImage.style.transform = `translate(${-100 * applePosition.x * appleStepPercentageX}%`;
+  }})
+};
 
-if (window.screen.width >= 600) {
-  tl.to(".bottom", { x: 500, duration: 2 }, "<");
-  tl.to(".draw", { x: 250, duration: 2 }, "<");
-  tl.to(".blow", { x: -250, duration: 2 }, "<");
-  tl.to(".top", { x: -500, duration: 2 }, "<");
-} else {
-  tl.to(".bottom", { x: 150, duration: 2 }, "<");
-  tl.to(".draw", { x: 75, duration: 2 }, "<");
-  tl.to(".blow", { x: -75, duration: 2 }, "<");
-  tl.to(".top", { x: -150, duration: 2 }, "<");
-}
-
-tl.to(".bottom", { x: 0, duration: 2 }, "<10");
-tl.to(".draw", { x: 0, duration: 2 }, "<");
-tl.to(".blow", { x: 0, duration: 2 }, "<");
-tl.to(".top", { x: 0, duration: 2 }, "<");
-
-if (window.screen.width >= 600) {
-  tl.fromTo(".tremolo", { x: 0 }, { x: 300 }, ">");
-  tl.fromTo(".diatonic", { x: 0 }, { x: 30 }, ">");
-  tl.fromTo(".chromatic", { x: 0 }, { x: -300 }, ">");
-} else {
-  tl.fromTo(".tremolo", { x: 0 }, { x: 75 }, ">");
-  tl.fromTo(".diatonic", { x: 0 }, { x: 0 }, ">");
-  tl.fromTo(".chromatic", { x: 0 }, { x: -75 }, ">");
-}
-
-tl.fromTo(".parts", { opacity: 1, duration: 0 }, { opacity: 0, y:200, duration: 5 }, ">");
-tl.fromTo(".kind", { opacity: 0, duration: 5, y: -200 }, { opacity: 1, duration: 5, y: 0 }, "<");
+init();
