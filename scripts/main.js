a79a4fddfea91ee783d510dc6491baceb24d2090
Tijmen <tijmen.koetse@student.howest.be>
2023-01-21 15:08:17 +0100

    script improved, no more repetition

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,40 @@
+
 import '/css/style.css'
 import "/css/nav.css";
 import "/css/responsive.css";
+import "/css/artists.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const harmonicaNumCols = 8;
-const harmonicaStepPercentageX = 1 / harmonicaNumCols;
-const harmonicaPositions = [
-  { x: 0 },
-  { x: 1 },
-  { x: 2 },
-  { x: 3 },
-  { x: 4 },
-  { x: 5 },
-  { x: 6 },
-  { x: 7 },
-];
+{
+let floating = true;
 
-const init = () => {
+const disableFloating = () => {
+  if (floating) {
+    floating = false;
+    const $harmonica = document.querySelector(".harmonica");
+    $harmonica.style.animation = "none";
+  } else {
+    floating = true;
+    const $harmonica = document.querySelector(".harmonica");
+    $harmonica.style.animation = "float 6s ease-in-out infinite";
+  }
+};
+
+const intro_part = () => {
+  const harmonicaNumCols = 8;
+  const harmonicaStepPercentageX = 1 / harmonicaNumCols;
+  const harmonicaPositions = [
+    { x: 0 },
+    { x: 1 },
+    { x: 2 },
+    { x: 3 },
+    { x: 4 },
+    { x: 5 },
+    { x: 6 },
+    { x: 7 },
+  ];
+
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".part_one",
@@ -32,15 +49,24 @@ const init = () => {
   };
   const $harmonicaImage = document.querySelector(".harmonica__image");
 
-  tl.to(harmonica, { step: harmonicaPositions.length-1, duration: 5, ease: SteppedEase.config(harmonicaPositions.length-1), onUpdate: () => {
-    console.log(harmonica.step);
-    const harmonicaPosition = harmonicaPositions[harmonica.step];
-    $harmonicaImage.style.transform = `translate(${-100 * harmonicaPosition.x * harmonicaStepPercentageX}%`;
-  }})
+  tl.to(harmonica, {
+    step: harmonicaPositions.length - 1,
+    duration: 5,
+    ease: SteppedEase.config(harmonicaPositions.length - 1),
+    onUpdate: () => {
+      console.log(harmonica.step);
+      const harmonicaPosition = harmonicaPositions[harmonica.step];
+      $harmonicaImage.style.transform = `translate(${
+        -100 * harmonicaPosition.x * harmonicaStepPercentageX
+      }%`;
+    },
+  });
   tl.fromTo(".intro", { opacity: 0 }, { opacity: 1 }), ">";
-  tl.call(disableFloating)
+  tl.call(disableFloating);
   tl.to(".harmonica", { opacity: 1 }), ">";
+};
 
+const deconstruct_part = () => {
   const tlParts = gsap.timeline({
     scrollTrigger: {
       trigger: ".parts",
@@ -54,59 +80,69 @@ const init = () => {
   tlParts.to(".draw", { x: 75 }, "<");
   tlParts.to(".blow", { x: -75 }, "<");
   tlParts.to(".top", { x: -150 }, "<");
+}
 
-};
+const artists_part = () => {
+  const artists = [
+    { name: "toots", startYear: 1900, endYear: 1955 },
+    { name: "bob", startYear: 1955, endYear: 1961 },
+    { name: "stevie", startYear: 1961, endYear: 1962 },
+    // { name: "west", startYear: 1962, endYear: 1967 },
+    // {name : "john", startYear : 1967, endYear : 1990},
+    // {name : "jason", startYear : 1990 , endYear : 2000},
+  ];
 
-let floating = true;
+  for (let artist in artists) {
+    let i = 0;
+    
+    let startCount = artists[artist].startYear,
+      num = { let: startCount };
 
-const disableFloating = () => {
-  if (floating) {
-    floating = false;
-    const $harmonica = document.querySelector(".harmonica");
-    $harmonica.style.animation = "none";
-  }
-  else {
-    floating = true;
-    const $harmonica = document.querySelector(".harmonica");
-    $harmonica.style.animation = "float 6s ease-in-out infinite";
+    const changeNumber = () => {
+    const $year = document.querySelector("." + artists[artist].name + "__year");
+    $year.innerHTML = num.let.toFixed();
+    };
+
+    const artistTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "." + artists[artist].name,
+        pin: true,
+        //markers: true,
+        start: "top 10%",
+        end: "+=4000",
+        scrub: true,
+      },
+    });
+    artistTl.to(num, { let: artists[artist].endYear, duration: 3, ease: "none", onUpdate: changeNumber, });
+
+    if (artist != 0) {
+      artistTl.fromTo("." + artists[artist].name + "__year", { opacity: 0 ,y: -500 }, { opacity: 1, y: 0 }, "<");
+    }
+
+    artistTl.fromTo("." + artists[artist].name+ "__name", { y: 800 }, { y: 0 }, "<");
+    artistTl.fromTo("." + artists[artist].name + "__nickname", { y: 800 }, { y: 0 }, "<1");
+    artistTl.fromTo("." + artists[artist].name + "__image", { y: 800 }, { y: 0 }, "<2");
+
+    artistTl.fromTo("." + artists[artist].name + "__name", { y: 0 }, { y: -500 }, "<4");
+    artistTl.fromTo("." + artists[artist].name + "__nickname", { y: 0 }, { y: -500 }, "<");
+
+    artistTl.fromTo("." + artists[artist].name + "__quote", { y: 800 }, { y: 0 }, "<6");
+
+    artistTl.fromTo("." + artists[artist].name + "__quote", { y: 0 }, { y: -500 }, "<2");
+    artistTl.fromTo("." + artists[artist].name + "__image", { opacity: 1 }, { opacity: 0, duration: 3 }, "<");
+    artistTl.fromTo("." + artists[artist].name + "__year", { y: 0 }, { y: -200 }, "<");
+
+    artistTl.fromTo("." + artists[artist].name +"__harmonica", { y: 100 }, { y: -500 }, "<2");
+    artistTl.fromTo("." + artists[artist].name + "__fact", { y: 100 }, { y: -500 }, "<");
+    artistTl.fromTo("." + artists[artist].name + "__work", { y: 100 }, { y: -500 }, "<");
   }
 }
 
-let startCount = 1900,
-  num = { var: startCount };
-
-const changeNumber = () => {
-  const $year = document.querySelector(".toots__year");
-  $year.innerHTML = num.var.toFixed();
-};
-
-const yearTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".toots",
-    pin: true,
-    //markers: true,
-    start: "top 10%",
-    end: "+=4000",
-    scrub: true,
-  },
-});
-  yearTl.to(num, { var: 1955, duration: 3, ease: "none", onUpdate: changeNumber, });
-  
-  yearTl.fromTo(".toots__name", { y: 800 }, { y: 0 }, "<");
-  yearTl.fromTo(".toots__nickname", { y: 800 }, { y: 0 }, "<1");
-  yearTl.fromTo(".toots__image", { y: 800 }, { y: 0 }, "<2");
-
-  yearTl.fromTo(".toots__name", { y: 0 }, { y: -500 }, "<4");
-  yearTl.fromTo(".toots__nickname", { y: 0 }, { y: -500 }, "<");
-
-  yearTl.fromTo(".toots__quote", { y: 800 }, { y: 0 }, "<6");
-
-  yearTl.fromTo(".toots__quote", { y: 0 }, { y: -500 }, "<2");
-  yearTl.fromTo(".toots__image", { opacity: 1 }, { opacity: 0, duration: 3}, "<");
-  yearTl.fromTo(".toots__year", { y: 0 }, { y: -200}, "<");
-
-  yearTl.fromTo(".toots__harmonica", { y: 100 }, { y: -500 }, "<2");
-  yearTl.fromTo(".toots__fact", { y: 100 }, { y: -500 }, "<");
-  yearTl.fromTo(".toots__work", { y: 100 }, { y: -500 }, "<");
+const init = () => {
+  intro_part();
+  deconstruct_part();
+  artists_part();
+}
 
 init();
+}
